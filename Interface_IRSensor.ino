17b30528f7ee1ff6cfedfc10fd10a329bb8ce366
NeoSky <naveenl@andrew.cmu.edu>
2019-06-11 04:00:06 -0400

    Add files via upload

---
+++ Interface_IRSensor.ino
@@ -0,0 +1,19 @@
+#include <IRremote.h>
+
+int RECV_PIN = 11;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void setup()
+{ 
+  irrecv.enableIRIn(); // Start the receiver
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    delay(900);
+    irrecv.resume(); // Receive the next value
+  }
+}
